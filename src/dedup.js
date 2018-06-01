11381973e5d36dc2ac2617b00617dde78b80b446
Alex Grasley <alex@grasley.com>
2018-05-31 22:22:27 -0700

    comment dedup fn

--- src/dedup.js
+++ src/dedup.js
@@ -1,3 +1,10 @@
+/**
+ * deduplicate an array
+ * @param {function(T1):string} toKey - Function to convert items in the array to a key.
+ * @param {function(T1,T1):boolean} compare - Comparison function for duplicate items.
+ * Returns true if the left operand should be kept.
+ * @param {array<T1>} xs - The array to deduplicate.
+ */
 export const dedup = (toKey, compare, xs) => {
     const map = {}
     xs.forEach((x, i) => {
