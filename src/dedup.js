c8a44e4dc069866e5d8b6b70bd1911d644d75569
Alex Grasley <alex@grasley.com>
2018-06-01 23:28:55 -0700

    add logging support

--- src/dedup.js
+++ src/dedup.js
@@ -11,10 +11,7 @@ export const dedup = (toKey, compare, xs) => {
     xs.forEach((x, i) => {
         const key = toKey(x) // get the key for the item
         const existing = map[key] // get any existing item for that key
-        if (existing && compare(existing, x)) { // keep the existing item
-            // TODO: logging
-        } else {
-            // TODO: logging
+        if (!(existing && compare(existing, x))) {
             map[key] = x // replace the existing item
         }
     })
@@ -59,18 +56,23 @@ export class Record {
     /**
      * compare records
      * @param {Record} other - Other record to compare
+     * @param {?Logger} logger
      * @returns {boolean} - True if this Record should be kept.
      */
-    keep (other){
+    keep (other, logger){
         const thisDate = Date.parse(this.record.entryDate)
         const otherDate = Date.parse(other.record.entryDate)
         if (thisDate > otherDate) { // this is newer
+            if (logger) logger.replaces(other, this)
             return true
         } else if (thisDate < otherDate) { // other is newer
+            if (logger) logger.replaces(this, other)
             return false
         } else if (this.idx > other.idx) { // this came after other
+            if (logger) logger.replaces(other, this)
             return true
         } else { // other came after this
+            if (logger) logger.replaces(this, other)
             return false
         }
     }
@@ -78,11 +80,92 @@ export class Record {
     /**
      * deduplicate an array of records
      * @param {array<Record>} recs - The records to deduplicate
+     * @param {?Logger} logger
      * @returns {array<Record>} - The deduplicated records
      */
-    static dedup(recs) {
-        const recs_ = dedup(r => r.record._id, (x,y) => x.keep(y), recs) // dedup by id
-        return dedup(r => r.record.email, (x,y) => x.keep(y), recs_) // dedup by email
+    static dedup(recs, logger) {
+        const recs_ = dedup(r => r.record._id, (x,y) => x.keep(y, logger), recs) // dedup by id
+        return dedup(r => r.record.email, (x,y) => x.keep(y, logger), recs_) // dedup by email
     }
 }
 
+/** Handles logging */
+export class Logger {
+
+    /**
+     * create a new logger
+     * @returns {Logger}
+     */
+    constructor() {
+        this.records = []
+        this.mapping = []
+    }
+
+    /**
+     * logs when the second argument replaces the first
+     * @param {Record} x
+     * @param {Record} y
+     */
+    replaces(x,y) {
+        this.mapping[x.idx] = y.idx
+    }
+
+    /**
+     * produce the logging string output
+     * @returns {string}
+     */
+    logChanges() {
+
+        /**
+         * get the index of the output record for a source record
+         * @param {number} x - The source record index
+         * @returns {number} - The output record index
+         */
+        const getOutput = x => {
+            let outIdx = x
+            while (this.mapping[outIdx]) {
+                outIdx = this.mapping[outIdx]
+            }
+            return outIdx
+        }
+
+        /**
+         * log the changes for fields of a record
+         * @param {Object} src - Source record
+         * @param {Object} out - Output record
+         * @returns {string}
+         */
+        const logFields = (src, out) => {
+            let s = ''
+            Object.keys(src).forEach(k => {
+                const srcVal = src[k]
+                const outVal = out[k]
+                if (srcVal === outVal) {
+                    s += `field "${k}": unchanged\n`
+                } else {
+                    s += `field "${k}": ${srcVal} ==> ${outVal}\n`
+                }
+            })
+            return s
+        }
+
+        /**
+         * log the change for an individual source record
+         * @param {Record} x - The source record
+         * @returns {string}
+         */
+        const logChange = x => {
+            const out = this.records[getOutput(x.idx)]
+            return `SOURCE RECORD ${x.idx+1}:`
+                 + `\n\n${JSON.stringify(x.record, null, 2)}`
+                 + `\n\nOUTPUT RECORD:`
+                 + `\n\n${JSON.stringify(out.record, null, 2)}`
+                 + `\n\nFIELD CHANGES:`
+                 + `\n\n${logFields(x.record,out.record)}\n`
+        }
+
+        let s = ''
+        this.records.forEach(x => { s += logChange(x) })
+        return s
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import program from 'commander'
 import path from 'path'
 import fs from 'fs-extra'
 
-import { Record } from './dedup'
+import { Record, Logger } from './dedup'
 
 /**
  * Format a path from the command line args
@@ -31,15 +31,25 @@ program
             '../outbox/log.txt')
     .parse(process.argv)
 
+const logger = new Logger()
 const deduped = fs.readFile(formatPath(program.input))
     .then(data => {
-        return Record.dedup(Record.fromJSON(data))
+        const recs = Record.fromJSON(data)
+        logger.records = recs
+        return Record.dedup(recs, logger)
     })
 
 const writePath = formatPath(program.output)
+const logPath = formatPath(program.log)
 const outDir = fs.ensureDir(path.dirname(writePath))
+const logDir = fs.ensureDir(path.dirname(logPath))
 
 Promise.all([deduped, outDir])
     .then(values => {
         return fs.writeFile(writePath, Record.toJSON(values[0]))
+    })
+
+Promise.all([deduped, logDir])
+    .then(values => {
+        return fs.writeFile(logPath, logger.logChanges())
     })
